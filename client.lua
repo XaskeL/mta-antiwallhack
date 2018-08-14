37a49a2e5bb78dea744b936f49d066154ee5f54a
XaskeL <37407928+XaskeL@users.noreply.github.com>
2018-08-15 03:23:30 +0400

    Add files via upload

--- client.lua
+++ client.lua
@@ -17,6 +17,8 @@ local centerx, centery		= guiGetScreenSize() -- center of screen
 
 local math.sqrt				= math.sqrt
 
+local g_bIsPlayerInCross 	= false
+
 local function getPlayers(bInStream)
 	return getElementsByType('player', root, bInStream)
 end
@@ -33,13 +35,21 @@ addEventHandler( 'onClientResourceStart', resourceRoot,
 			return
 		end
 	
+		g_bIsPlayerInCross = false
+		
 		if bCheckOnCamera then
 			if isPlayerAiming(localPlayer) then
 				for i, player in ipairs( getPlayers(true) ) do
-					local x,y,z 	= getElementPosition(player)
-					local coords 	= { getScreenFromWorldPosition ( x, y, z ) }
-					if math.sqrt( (coords[1] - centerx) ^ 2 + (coords[2] - centery) ^ 2) < iFovCheckOnCamera then
-
+					local x,y,z = getElementPosition(player)
+					for i = 0, 2 do -- 3
+						local coords = { getScreenFromWorldPosition ( x, y, z + (0.25 * i) ) }
+						if math.sqrt( (coords[1] - centerx) ^ 2 + (coords[2] - centery) ^ 2) < iFovCheckOnCamera then
+							g_bIsPlayerInCross = true
+						end
+					end
+					if g_bIsPlayerInCross then 
+						
+						break 
 					end
 				end
 			end
