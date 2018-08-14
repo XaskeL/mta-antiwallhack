c248f42429901510b860ec8f62b48df2fc8699bd
XaskeL <37407928+XaskeL@users.noreply.github.com>
2018-08-15 03:09:55 +0400

    Add files via upload

--- client.lua
+++ client.lua
@@ -3,3 +3,60 @@
 -- Author: XaskeL
 -- it so easy!
 -----------------------------------
+local iDelayUpdate 			= 0
+local iDelayMS 				= 8
+
+local getElementsByType		= getElementsByType
+local getTickCount 			= getTickCount
+local getPlayerWeapon 		= getPlayerWeapon
+local getPedTask			= getPedTask
+local getElementPosition 	= getElementPosition
+
+local centerx, centery 		= guiGetScreenSize() -- center of screen
+	  centerx, centery		= centerx / 2, centery / 2
+
+local math.sqrt 			= math.sqrt
+
+local function getPlayers(bInStream)
+	return getElementsByType('player', root, bInStream)
+end
+
+local function isPlayerAiming(player)
+	if getPedTask(player, 'secondary', 0) == 'TASK_SIMPLE_USE_GUN' then
+		return true
+	end
+end
+
+addEventHandler( 'onClientResourceStart', resourceRoot, 
+	function()
+		if getPlayerWeapon(localPlayer) < 1 then
+			return
+		end
+	
+		if bCheckOnCamera then
+			if isPlayerAiming(localPlayer) then
+				for i, player in ipairs( getPlayers(true) ) do
+					local x,y,z 	= getElementPosition(player)
+					local coords 	= { getScreenFromWorldPosition ( x, y, z ) }
+					if math.sqrt( (coords[1] - centerx) ^ 2 + (coords[2] - centery) ^ 2) < iFovCheckOnCamera then
+
+					end
+				end
+			end
+		end
+		
+	end
+);
+
+local function underTheSelf()
+	
+end
+
+addEventHandler( 'onClientRender', resourceRoot,
+	function()
+		if getTickCount() > iDelayUpdate then
+			iDelayUpdate = getTickCount() + iDelayMS -- MAX_FPS = 100; 1000/MAX_FPS=10 MS; 1000 / (10 + 8) = 55 UPDATES IN SECOND
+			underTheSelf()
+		end
+	end
+);
---
+++ settings.lua
@@ -0,0 +1,10 @@
+﻿-- CONFIG FILE --
+iCountsWarning 				= 5;
+iFovCheckOnCamera 			= 1;
+
+bSimulatePlayers 			= true; -- simulating players on stream. BAD: CPedPool (Memory Address: 0x00B7CD98)
+bSimulateVehicles 			= true; -- simulating vehicles on stream. BAD: CPedPool (Memory Address: 0xC502AA0)
+bCheckAdmins				= true; -- system checking players on hack (shader! invisible players!) /checkwh (nick)
+bCheckOnCamera 				= true; -- warnings for admins if player from wall of aimed on player
+bCheckOnFiring 				= true; -- warnings (+iCountsWarning counts!) for admins, if warnings > 5 = ban on week. If player from wall firing of on player
+-- END
